ee08141215ad8407387763efa7b94a74a424822c
Björn Molin <bjmo21@student.bth.se>
2022-04-01 16:14:51 +0100

    report first

---
+++ src/Controller/LuckyControllerTwig.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyControllerTwig extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function start(): Response
+    {
+        $number = random_int(0, 100);
+        $title = "Home";
+
+        return $this->render('start.html.twig', [
+            'number' => $number,
+            'title' => $title,
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        $title = "About";
+
+        return $this->render('about.html.twig', [
+            'title' => $title,
+        ]);
+    }
+
+    /**
+     * @Route("/report", name="report")
+     */
+    public function report(): Response
+    {
+        $title = "Report";
+
+        return $this->render('report.html.twig', [
+            'title' => $title,
+        ]);
+    }
+}
